11495bac70502d297d5528c249e5ad22e67cb2a6
Francesco Gorini <francocecco97@gmail.com>
2020-07-03 11:13:40 +0200

    Modified usersControlle.js, tryed to fix a bug

--- server/app/src/controllers/usersController.js
+++ server/app/src/controllers/usersController.js
@@ -25,24 +25,16 @@ exports.read_user = function(req, res) {
 
 // POST
 exports.create_user = function(req, res) {
-    const user = new User({
-        name: req.body.name,
-        surname: req.body.surname,
-        phone: req.body.phone,
-        email: req.body.email,
-        address: req.body.address,
-        registered: req.body.registered,
-    });
+    var newUser = new User(req.body);
 
     // 201 -> istance created
-    user.save((saveErr, savedUser) => {
-        if (saveErr) {
-
-        } else {
-            console.log("User theorically created")
-            res.status(201).send({ data: savedUser });
-            res.json(savedUser);
+    newUser.save(function(err, user) => {
+        if (err) {
+            res.send(err);
         }
+
+        res.status(201).send({ data: savedUser });
+        res.json(savedUser);
     });
 };
 
