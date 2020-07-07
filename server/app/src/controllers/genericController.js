a9793fb31384b7aad5e504b9a45710d02a0af4ef
Thomas Angelini <thomas.angelini@studio.unibo.it>
2020-07-07 12:18:46 +0200

    Added a common function

--- server/app/src/controllers/genericController.js
+++ server/app/src/controllers/genericController.js
@@ -5,3 +5,12 @@ exports.serve_client_app = function(req, res) {
 exports.serve_plain_404 = function(req, res) {
 	res.status(404).send({url: req.originalUrl + ' not found'})
 };
+
+module.exports.correct_save = function (user, status, res) {
+		user.save((saveErr, updatedUser) => {
+			if (saveErr) {
+				res.send(saveErr);
+			}
+			res.status(200).json(updatedUser);
+		});
+	}
