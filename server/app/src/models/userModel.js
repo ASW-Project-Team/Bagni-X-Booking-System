aaca7060033fc94e2a56f6dfe5813411294c447c
Thomas Angelini <thomas.angelini@studio.unibo.it>
2020-07-07 11:16:51 +0200

    User fixed for admin

--- server/app/src/models/userModel.js
+++ server/app/src/models/userModel.js
@@ -15,12 +15,12 @@ const initializeModel = function(mongoose) {
     // it's possible also nested declaration?
     let userSchema = new Schema({
         _id: mongoose.Types.ObjectId,
-        name: String, // String is shorthand for {type: String}
+        name: String,
         surname: String,
-        phone: String, // fixme add validator from server
+        phone: {type: String, default: null}, // fixme add validator from server
         email: String, // fixme add validator from server
-        address: String,
-        registered: Boolean,
+        address: {type: String, default: null},
+        registered: {type: Boolean, default: true},
         admin: {type: Boolean, default: false},
         deleted: {type: Boolean, default: false},
     });
