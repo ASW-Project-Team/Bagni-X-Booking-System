0a2812768e157249f0af6888ae1de08dc4240a87
Thomas Angelini <thomas.angelini@studio.unibo.it>
2020-07-08 11:16:17 +0200

    The possibility for more than umbrella

--- server/app/src/models/bookingModel.js
+++ server/app/src/models/bookingModel.js
@@ -15,13 +15,13 @@ const initializeModel = function(mongoose) {
     const bookingSchema = new Schema({
         _id: Schema.Types.ObjectID,
         user_id: Schema.Types.ObjectID,
-        umbrella_id: Schema.Types.ObjectID,
+        umbrella_id: [Schema.Types.ObjectID],
         confirmed: {type: Boolean, default: false},
         cancelled: {type: Boolean, default: false},
         price: {type: Float, $gt: 0.0}, // fixme price > price min
         date_from: {type: Date, $gte: Date.now()}, // fixme $gte not correct
         date_to: {type: Date, $gte: Date.now()}, // fixme date_to > date_from
-        services: [{type: serviceModel, default: null}]
+        services: [{type: serviceModel, default: null}] // default beach volley
     });
 
     return mongoose.model('booking', bookingSchema, 'bookings');
