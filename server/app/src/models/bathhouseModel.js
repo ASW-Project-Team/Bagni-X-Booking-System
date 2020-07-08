6ca47d3eb5a7b6ea9d3e3eb63e823378c8fbf659
riccardomaldini <riccardo.maldini@gmail.com>
2020-07-08 10:49:08 +0200

    Customization home api

--- server/app/src/models/bathhouseModel.js
+++ server/app/src/models/bathhouseModel.js
@@ -9,22 +9,13 @@ module.exports = function(mongoose) {
 
 
 const initializeModel = function(mongoose) {
-    const galleryModel = require('./nestedSchemas/galleryModel')(mongoose).schema;
-
     let Schema = mongoose.Schema;
     let bathhouseInfo = new Schema({
         _id: Schema.Types.ObjectId,
         name: String, // String is shorthand for {type: String}
         logo_url: String,
+        main_home_card: null, // todo
         home_cards: null, // todo list of home cards
-        description: {type: String, default: ""},
-        phone: String, // fixme add validator from server
-        n_umbrella: {
-            type: Number,
-            min: [10, 'Too small'],
-        },
-        n_available_now_umbrellas: Number,
-        gallery: [{ type: galleryModel, default: null}]
     });
     return mongoose.model('bathhouse_info', bathhouseInfo, 'bathhouse_info');
 }
