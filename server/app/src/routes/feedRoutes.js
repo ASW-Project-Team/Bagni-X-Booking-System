8505e8e8de32f3dc55f3d834c57fb771eba6154d
Thomas Angelini <thomas.angelini@studio.unibo.it>
2020-08-14 17:22:52 +0200

    Added delete e post

--- server/app/src/routes/feedRoutes.js
+++ server/app/src/routes/feedRoutes.js
@@ -2,6 +2,8 @@ module.exports.set = function(app) {
 	// due to not possible two or more require at the same collection
 	const feedController = require('../controllers/feedController');
 
-	app.route('/api/feed/:id')
+	app.route('/api/feed/')
 		.get(feedController.read_feed)
+		.delete(feedController.delete_feed)
+		.post(feedController.create_feed);
 };
