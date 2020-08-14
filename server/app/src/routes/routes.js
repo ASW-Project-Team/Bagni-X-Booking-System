036877f4f955cd70c809813542344b2f941268dd
Thomas Angelini <thomas.angelini@studio.unibo.it>
2020-08-14 17:22:39 +0200

    Added feed

--- server/app/src/routes/routes.js
+++ server/app/src/routes/routes.js
@@ -3,11 +3,14 @@ module.exports.set = function(app) {
 	const bookingRoutes = require('./bookingsRoutes');
 	const genericRoutes = require('./genericRoutes');
 	const catalogRoutes = require('./catalogRoutes');
+	const feedRoutes = require('./feedRoutes');
 	// todo add all required root here
 
 	usersRoutes.set(app);
 	bookingRoutes.set(app);
 	catalogRoutes.set(app);
+	feedRoutes.set(app);
+
 
 	// the generic routes setting must be called last, handling all
 	// remaining routes
