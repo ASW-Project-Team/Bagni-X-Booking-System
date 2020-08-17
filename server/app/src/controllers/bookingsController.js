469411c2a6878f0b8397e450dbce76f9143c5139
Thomas Angelini <thomas.angelini@studio.unibo.it>
2020-08-17 13:02:00 +0200

    Refactor used in commonController

--- server/app/src/controllers/bookingsController.js
+++ server/app/src/controllers/bookingsController.js
@@ -4,7 +4,6 @@ const Booking = require("../models/bookingModel")(mongoose);
 const commonController = require("./commonController");
 
 
-const ObjectId = mongoose.Schema.Types.ObjectID
 const collectionName = "bookings"
 
 
@@ -29,23 +28,57 @@ exports.create_booking = function(req, res) {
 	}, req.body.user_id, req.body.umbrella_id, req.body.price, req.body.date_from, req.body.date_to);
 };
 
-// OK
-// It lists all bookings
-exports.list_bookings = function(req, res) {
-		Booking.find({}, function(err, feed) {
-			if (err){
-				res.send(err);
-			}
-			res.json(feed);
-		});
-};
-
-
-// FIXME TO CHECK
 /**
- *
- * @param req
- * @param res
+ * Get the booking specified in id
+ * @param req The GET specific booking request
+ * @param res The GET specific booking response
+ */
+exports.get_booking = function(req, res) {
+	commonController.findByIdFirstLevelCollection(req, res, "book", Booking, "book not found",
+		req.params.id, (err, docResult)=>{
+			commonController.response(res, docResult);
+		});
+}
+
+exports.modify_booking = function(req, res) {
+
+	commonController.findByIdFirstLevelCollection(req, res, "book", Booking, "book not found",
+		req.params.id, (err, docResult)=>{
+
+			if (req.body.user_id)
+				docResult.user_id = req.body.user_id
+
+			if (req.body.umbrella_id)
+				docResult.umbrella_id = req.body.umbrella_id
+
+			if (req.body.confirmed)
+				docResult.confirmed = req.body.confirmed
+
+			if (req.body.cancelled)
+				docResult.cancelled = req.body.cancelled
+
+			if ((req.body.date_from) && (req.body.date_from.getTime() > Date.now()))
+				docResult.date_from = req.body.date_from
+
+			if ((req.body.date_to) && (req.body.date_to.getTime() > docResult.date_from.getTime()))
+					docResult.date_to = req.body.date_to
+
+			if (req.body.services)
+				docResult.services = req.body.services
+
+			commonController.correctSave(docResult, commonController.status_completed, res);
+		})
+}
+
+
+/**
+ * GET bookings.
+ * Two possible scenarios:
+ * 	. get a specific user and get all his bookings
+ * 	. get page_id and page_size: in this case return bookings from id to size.
+ * 		In this case there are default value both for id and size.
+ * @param req The specified GET request
+ * @param res The specified GET response
  */
 exports.read_bookings = function(req, res) {
 
@@ -59,152 +92,12 @@ exports.read_bookings = function(req, res) {
 
 }
 
-// OK
-// It returns the booking with the specified ID
-exports.read_booking = function(req, res) {
-
-
-	/*commonController.checkFirstLevelClass(req, res, "Booking", Booking, "Booking not found",
-		mongoose.Types.ObjectId(req.params.id), (err, document) =>{
-			commonController.response(res, document);
-		}
-	);*/
-
-	Booking.findById(req.params.id, (err, booking) => {
-		if (err)
-			res.send(err);
-		else{
-			if(booking == null) {
-				res.status(404).send({
-					description: 'Booking not found'
-				});
-			} else {
-				res.status(200).json(booking);
-			}
-		}
-	});
-};
-
-
-
-exports.update_booking = function(req, res) {
-	// todo uguale, solo che poi sovrascrivi
-
-	User.findById(mongoose.Types.ObjectId(req.params.id), (errUser, user) => {
-		if (errUser)
-			res.send(errUser);
-		else{
-			if(user == null){
-				res.status(404).send({
-					description: 'User not found'
-				});
-			}
-			else{
-
-				// FIXME aggregate
-
-				let booking = user.bookings.find(correctId);
-
-				if (booking !== undefined) {
-
-					if (req.body.umbrella_id !== undefined)
-						booking.umbrella_id = mongoose.Types.ObjectId(req.body.umbrella_id);
-
-					if (req.body.price !== undefined)
-						booking.price = req.body.price;
-
-					if (req.body.date_from !== undefined)
-						booking.date_from = req.body.date_from;
-
-					if (req.body.date_to !== undefined)
-						booking.date_to = req.body.date_to;
-
-					user.save((saveErr, updatedUser) => {
-						if (saveErr) {
-							res.send(saveErr)
-						}
-
-						res.status(200).json(updatedUser);
-					});
-				} else {
-					res.status(404).send("Booking not found");
-				}
-			}
-		}
-	});
-};
-
-function correctId(reqId, bookingId) {
-	return reqId === bookingId;
-}
-
-// OK
-// It deletes a booking
+/**
+ * DELETE Booking specified by id
+ * @param req The specific DELETE request
+ * @param res The specific DELETE response
+ */
 exports.delete_booking = function(req, res) {
-	Booking.deleteOne({_id: req.params.id}, function(err, result) {
-		if (err)
-			res.send(err);
-		else{
-			if(result.deletedCount === 0){
-				res.status(404).send({
-					description: 'Booking not found'
-				});
-			}
-			else{
-				res.json({ message: 'Task successfully deleted' });
-			}
-		}
-	});
-};
 
-function read_bookings_from_user(res, user_id, max_page_size, page_id) {
-	let skip_value = page_id * max_page_size;
-	let limit_value = (page_id + 1) * max_page_size;
-	User.findById(ObjectId(user_id)
-		.sort({'bookings.date_from': 'desc'})
-		.limit(limit_value)
-		.skip(skip_value), function (err, user) {
-		if (err)
-			res.send(err);
-		else {
-			if (user == null) {
-				res.status(404).send({ // fixme better 404
-					description: 'User or bookings not found'
-				});
-			} else {
-				res.status(200);
-				res.json();
-			}
-		}
-	});
-	/*User.findById(ObjectId(id), function(err, user) {
-        if (err)
-            res.send(err);
-        else {
-            if (user == null) {
-                res.status(404).send({ // fixme better 404
-                    description: 'User not found'
-                });
-            } else {
-                // responds with user
-                let skip_value = page_id * max_page_size;
-                let limit_value = (page_id + 1) * max_page_size;
-                let bookingsRequested = User.find()
-                    .sort({'bookings.date_from': 'desc'})
-                    .limit(limit_value)
-                    .skip(skip_value)
-                    .exec() // Return the promise
-                    .values // Return all the values
-                    .then(function (bookings) {
-                        res.status(200);
-                        res.json(bookingsRequested);
-                    });
-
-
-                res.status(200);
-                res.json(bookingsRequested);
-            }
-        }
-    });*/
-
+	commonController.deleteFirstLevelCollection(req, res, collectionName, Booking, "", req.params.id);
 }
