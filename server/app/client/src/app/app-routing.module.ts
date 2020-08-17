0d293650af9306e8820f79105d22385d05214aa0
riccardomaldini <riccardo.maldini@gmail.com>
2020-08-17 20:01:22 +0200

    Added some basic page structure and routing

--- server/app/client/src/app/app-routing.module.ts
+++ server/app/client/src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { HomeComponent } from "./customer/home/home.component";
+import { FeedComponent } from "./customer/feed/feed.component";
+import { BookingsComponent } from "./customer/bookings/bookings.component";
+import { ProfileComponent } from "./customer/profile/profile.component";
 
-const routes: Routes = [];
+// populate app routes
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'feed', component: FeedComponent },
+  { path: 'bookings', component: BookingsComponent },
+  { path: 'profile', component: ProfileComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- server/app/client/src/app/app.module.ts
+++ server/app/client/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './customer/home/home.component';
+import { FeedComponent } from './customer/feed/feed.component';
+import { BookingsComponent } from './customer/bookings/bookings.component';
+import { ProfileComponent } from './customer/profile/profile.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    FeedComponent,
+    BookingsComponent,
+    ProfileComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
