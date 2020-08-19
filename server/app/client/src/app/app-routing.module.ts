2a38fcfd7a5648944769c2e3178bd2310168b8cd
riccardomaldini <riccardo.maldini@gmail.com>
2020-08-19 16:38:12 +0200

    added basic 404 feature, some refactoring

--- server/app/client/src/app/app-routing.module.ts
+++ server/app/client/src/app/app-routing.module.ts
@@ -5,13 +5,16 @@ import { HomeComponent } from "./customer/home/home.component";
 import { FeedComponent } from "./customer/feed/feed.component";
 import { BookingsComponent } from "./customer/bookings/bookings.component";
 import { ProfileComponent } from "./customer/profile/profile.component";
+import { PageNotFoundComponent } from "./page-structure/page-not-found/page-not-found.component";
 
 // populate app routes
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'feed', component: FeedComponent },
   { path: 'bookings', component: BookingsComponent },
-  { path: 'profile', component: ProfileComponent }
+  { path: 'profile', component: ProfileComponent },
+  { path: '404', component: PageNotFoundComponent },
+  { path: '**', redirectTo: '/404' }
 ];
 
 @NgModule({
--- server/app/client/src/app/app.module.ts
+++ server/app/client/src/app/app.module.ts
@@ -7,8 +7,8 @@ import { HomeComponent } from './customer/home/home.component';
 import { FeedComponent } from './customer/feed/feed.component';
 import { BookingsComponent } from './customer/bookings/bookings.component';
 import { ProfileComponent } from './customer/profile/profile.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './page-structure/header/header.component';
+import { FooterComponent } from './page-structure/footer/footer.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -17,6 +17,7 @@ import { DomSanitizer } from '@angular/platform-browser';
 import {MatMenuModule} from '@angular/material/menu';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import { HttpClientModule } from '@angular/common/http';
+import { PageNotFoundComponent } from './page-structure/page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
@@ -27,6 +28,7 @@ import { HttpClientModule } from '@angular/common/http';
     ProfileComponent,
     HeaderComponent,
     FooterComponent,
+    PageNotFoundComponent,
   ],
   imports: [
     BrowserModule,
