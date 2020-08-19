5308b8b19499ce978aace36882c9f3fba02ab879
Thomas Angelini <thomas.angelini@studio.unibo.it>
2020-08-19 11:55:17 +0200

    Added right parameters with some more checks

--- server/app/src/routes/usersRoutes.js
+++ server/app/src/routes/usersRoutes.js
@@ -7,15 +7,6 @@ module.exports.set = function(app) {
 
     app.route('/api/customers/')
         .post(usersController.create_user);
-    /* used: {
-    "name": "userName",
-    "surname": "userSurname",
-    "phone": "000000000",
-    "email": "user.email@gmail.com",
-    "address": "addressUser",
-    "registered": true
-    }*/
-
 
 
 
