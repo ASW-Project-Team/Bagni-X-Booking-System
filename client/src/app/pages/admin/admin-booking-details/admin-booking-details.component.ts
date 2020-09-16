d3bf4a66e0d6c3d69ec32910330326a23a801c99
riccardomaldini <riccardo.maldini@gmail.com>
2020-09-16 16:40:31 +0200

    Back on confirmed booking

--- client/src/app/pages/admin/admin-booking-details/admin-booking-details.component.ts
+++ client/src/app/pages/admin/admin-booking-details/admin-booking-details.component.ts
@@ -1,11 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import {Booking, BookingModel} from "../../../shared/models/booking.model";
+import {Booking} from "../../../shared/models/booking.model";
 import {AppbarAction} from "../../../shared/components/appbars/appbars.model";
 import {ActivatedRoute, Router} from "@angular/router";
 import {ApiService} from "../../../core/api/api.service";
 import {MatSnackBar} from "@angular/material/snack-bar";
 import {MatDialog} from "@angular/material/dialog";
-import {DateUtils} from "../../../shared/utils/date.utils";
 import {AlertDialogComponent} from "../../../shared/components/alert-dialog/alert-dialog.component";
 import {Customer} from "../../../shared/models/customer.model";
 
@@ -16,95 +15,77 @@ import {Customer} from "../../../shared/models/customer.model";
 })
 export class AdminBookingDetailsComponent implements OnInit {
   // params
-  bookingId: string;
-  bookingTitle: string;
+  titleParam: string;
 
-  downloadedBooking: Booking;
+  booking: Booking;
   customer: Customer;
   actions: AppbarAction[] = [];
 
   constructor(private route: ActivatedRoute,
               private api: ApiService,
               private snackBar: MatSnackBar,
-              private dialog: MatDialog) { }
+              private dialog: MatDialog,
+              private router: Router) { }
 
   ngOnInit(): void {
     this.route.params.subscribe(params => {
-      this.bookingId = params.id;
-      this.bookingTitle = params.title;
+      this.titleParam = params.title;
 
-      this.api.getBooking(this.bookingId).subscribe(data => {
-        this.downloadedBooking = new Booking(data);
+      this.api.getBooking(params.id).subscribe(data => {
+        this.booking = new Booking(data);
 
-        this.api.getCustomer(this.downloadedBooking.customerId).subscribe(data => {
+        this.api.getCustomer(this.booking.customerId).subscribe(data => {
           this.customer = new Customer(data);
         });
 
-        let context = this;
-        this.actions.push({
-          id: "0",
-          name: "Annulla prenotazione",
-          mdiIcon: 'trash-can-outline',
-          isMdi: true,
-          disabled: this.downloadedBooking.cancelled,
-          execute: function () {
-            context.dialog.open(AlertDialogComponent, {
-              data: {
-                content: "Sei sicuro di voler annullare la tua prenotazione? L'azione non è reversibile.",
-                positiveAction: {
-                  text: "Sì, annulla",
-                  execute: function() {
-                    context.api.editBooking(context.bookingId, { cancelled: true }).subscribe(() => {
-                      context.updateBooking();
-                      context.snackBar.open("Prenotazione annullata.", null, { duration: 4000 });
-                    });
-                  }
-                },
-                negativeAction: {
-                  text: "No",
-                  execute: function() { }
+        let ctx = this;
+        this.actions = [
+          {
+            id: "0",
+            name: "Annulla prenotazione",
+            mdiIcon: 'trash-can-outline',
+            isMdi: true,
+            disabled: this.booking.cancelled,
+            execute: () => this.dialog.open(AlertDialogComponent, { data: {
+              content: "Sei sicuro di voler annullare la tua prenotazione? L'azione non è reversibile.",
+              negativeAction: { text: "No", execute: () => { } },
+              positiveAction: {
+                text: "Sì, annulla",
+                execute: () => {
+                  this.api.editBooking(ctx.booking.id, { cancelled: true })
+                    .subscribe(() => this.router.navigate(['/admin/bookings'])
+                      .then(() => this.snackBar.open("Prenotazione annullata.", null, { duration: 4000 }))
+                    )
                 }
               }
-            });
+            }})
+          },
+          {
+            id: "1",
+            name: "Conferma prenotazione",
+            mdiIcon: 'progress-check',
+            isMdi: true,
+            disabled: this.booking.confirmed,
+            execute: () => {
+              this.api.editBooking(ctx.booking.id, { confirmed: true })
+                .subscribe(() => this.router.navigate(['/admin/bookings'])
+                  .then(() => this.snackBar.open("Prenotazione confermata.", null, { duration: 4000 }))
+                )
+            }
           }
-        });
-
-        this.actions.push({
-          id: "1",
-          name: "Conferma prenotazione",
-          mdiIcon: 'progress-check',
-          isMdi: true,
-          disabled: this.downloadedBooking.confirmed,
-          execute: function () {
-            context.api.editBooking(context.bookingId, { confirmed: true }).subscribe(() => {
-              context.updateBooking();
-              context.snackBar.open("Prenotazione confermata.", null, { duration: 4000 });
-            });
-          }
-        });
+        ];
       });
     });
   }
-
-  private updateBooking() {
-    this.api.getBooking(this.bookingId).subscribe(data => {
-      this.downloadedBooking = new Booking(data);
-
-      this.api.getCustomer(this.downloadedBooking.customerId).subscribe(data => {
-        this.customer = new Customer(data);
-      });
-    });
-  }
-
 
   /**
    * Compute appbar title basing on the available information.
    */
   getAppbarTitle() : string {
-    if (this.downloadedBooking) {
-      return this.downloadedBooking.getTitle();
-    } else if (this.bookingTitle) {
-      return this.bookingTitle;
+    if (this.booking) {
+      return this.booking.getTitle();
+    } else if (this.titleParam) {
+      return this.titleParam;
     } else {
       return '';
     }
