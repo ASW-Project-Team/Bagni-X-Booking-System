1dd2f65a019c3e1f70f974ddd5db5ab443859b14
riccardomaldini <riccardo.maldini@gmail.com>
2020-09-21 13:13:52 +0200

    Path fix

--- client/src/app/core/mocks/bathhouse.mock.ts
+++ client/src/app/core/mocks/bathhouse.mock.ts
@@ -2,7 +2,7 @@ import {BathhouseModel} from "../../shared/models/bathhouse.model";
 
 export const bathhouseMock: BathhouseModel = {
   name: 'Bagni X',
-  logoUrl: '/assets/fake-backend/logo.png',
+  logoUrl: 'assets/fake-backend/logo.png',
   seasonStart: new Date("2020-05-10"),
   seasonEnd: new Date("2020-09-15"),
 }
