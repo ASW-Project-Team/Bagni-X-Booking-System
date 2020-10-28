4c3ca6fdf45a281b6cbcad20f67ce422e5701eb6
riccardomaldini <riccardo.maldini@gmail.com>
2020-10-28 16:04:34 +0100

    Resolved authorization problems with query params

--- server/src/utils/authentication.js
+++ server/src/utils/authentication.js
@@ -145,7 +145,7 @@ const parseToken = async (authHeader) => {
  */
 const getInlineParam = (url) => {
   const parts = url.split('/')
-  return parts[parts.length - 1]
+  return parts[parts.length - 1].split("?")[0]
 }
 
 
